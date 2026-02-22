1ea493afc68846bfddb3434639ccf9953479016b
Dani Veskov <daniveskov@gmail.com>
2026-02-22 20:37:23 +0200

    remove local .env for production

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const pool = require("./db");
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
 
 const express = require("express");
 const path = require("path");
